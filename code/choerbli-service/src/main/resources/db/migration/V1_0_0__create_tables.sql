5bf09143dd88649c7b4b61d50fec3800b55ca5e7
Julian Haldimann <julian@haldimann.be>
2025-08-23 01:38:36 +0200

    Add inital database tables.

---
+++ code/choerbli-service/src/main/resources/db/migration/V1_0_0__create_tables.sql
@@ -0,0 +1,86 @@
+create table if not exists choerbli (
+    id uuid not null,
+    organizer_id uuid not null,
+    state int not null,
+    name varchar(255),
+    description varchar(255),
+    end_date date,
+    start_date date,
+    primary key (id)
+);
+
+create table if not exists ch_user (
+    id uuid not null,
+    email varchar(255),
+    name varchar(255),
+    primary key (id)
+);
+
+create table if not exists category (
+    id uuid not null,
+    name varchar(255),
+    primary key (id)
+);
+
+create table if not exists item_description (
+    id uuid not null,
+    category_id uuid,
+    primary key (id)
+);
+
+create table if not exists consequences (
+    id uuid not null,
+    choerbli_id uuid,
+    description varchar(255),
+    primary key (id)
+);
+
+create table if not exists item (
+    id uuid not null,
+    choerbli_id uuid,
+    user_id uuid,
+    points int,
+    price float,
+    primary key (id)
+);
+
+create table if not exists user_rank (
+    id uuid not null,
+    user_id uuid,
+    choerbli_id uuid,
+    points int,
+    primary key (id)
+);
+
+create table if not exists vote (
+    id uuid not null,
+    factor int,
+    item_descriptor_id uuid,
+    user_id uuid,
+    choerbli_id uuid,
+    primary key (id)
+);
+
+alter table if exists vote
+    add constraint fk_user_id foreign key (user_id) references ch_user;
+
+alter table if exists vote
+    add constraint fk_choerbli_id foreign key (choerbli_id) references choerbli;
+
+alter table if exists user_rank
+    add constraint fk_choerbli_id foreign key (choerbli_id) references choerbli;
+
+alter table if exists item
+    add constraint fk_choerbli_id foreign key (choerbli_id) references choerbli;
+
+alter table if exists item
+    add constraint fk_user_id foreign key (user_id) references ch_user;
+
+alter table if exists consequences
+    add constraint fk_choerbli_id foreign key (choerbli_id) references choerbli;
+
+alter table if exists item_description
+    add constraint fk_category_id foreign key (category_id) references category;
+
+alter table if exists choerbli
+    add constraint organisor_id foreign key (organizer_id) references ch_user;
