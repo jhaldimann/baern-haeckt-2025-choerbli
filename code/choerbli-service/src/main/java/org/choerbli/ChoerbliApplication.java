a37c1c5d9c27be69740fe8d01b69c23e92f4cd2a
Tim Frey <frey.tim@outlook.com>
2025-08-22 21:35:14 +0200

    project setup

---
+++ code/choerbli-service/src/main/java/org/choerbli/ChoerbliApplication.java
@@ -0,0 +1,12 @@
+package org.choerbli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoerbliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoerbliApplication.class, args);
+    }
+}
