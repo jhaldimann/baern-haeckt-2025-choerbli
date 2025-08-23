8071fc99718dc487b94119b0e153ca999cb41cf5
Tim Frey <frey.tim@outlook.com>
2025-08-23 17:12:33 +0200

    you can now switch phases

--- code/choerbli-ui/src/app/pages/choerbli/choerbli.ts
+++ code/choerbli-ui/src/app/pages/choerbli/choerbli.ts
@@ -2,7 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {Item} from './item/item';
 import {AsyncPipe, LowerCasePipe} from '@angular/common';
 import {ItemDescriptionApiService} from '../../shared/services/item-description-api.service';
-import {BehaviorSubject, take} from 'rxjs';
+import {BehaviorSubject, finalize, take} from 'rxjs';
 import {ItemDescription} from '../../shared/models/item-description.model';
 import {ChoerbliStore} from '../../shared/stores/choerbli-store';
 import {UserStore} from '../../shared/stores/user-store';
@@ -12,6 +12,8 @@ import {User} from '../../shared/models/user.model';
 import {Card} from 'primeng/card';
 import {ChoerbliStatus} from '../../shared/models/choerbli.model';
 import {AssigningForm} from '../../shared/components/assigning-form/assigning-form';
+import {PhaseApiService} from '../../shared/services/phase-api.service';
+import {patchState} from '@ngrx/signals';
 
 @Component({
   selector: 'app-choerbli',
@@ -29,6 +31,7 @@ import {AssigningForm} from '../../shared/components/assigning-form/assigning-fo
 })
 export class Choerbli implements OnInit {
   itemDescriptionApiService: ItemDescriptionApiService = inject(ItemDescriptionApiService);
+  phaseApiService: PhaseApiService = inject(PhaseApiService);
   items: BehaviorSubject<ItemDescription[]> = new BehaviorSubject<ItemDescription[]>([]);
   id: string = '';
   readonly choerbliStore = inject(ChoerbliStore);
@@ -55,6 +58,16 @@ export class Choerbli implements OnInit {
   createUser() {
     this.user.choerbliId = this.choerbliStore.choerbli()?.id ?? '';
     this.userStore.createUser(this.user);
+  }
+
+  nextPhase() {
+    if (this.choerbliStore.choerbli().state === ChoerbliStatus.VOTING) {
+      this.phaseApiService.toAssigningPhase(this.id).pipe(finalize(() => this.choerbliStore.loadChoerbliById(this.id)),).subscribe(() => {
+      });
+    } else if (this.choerbliStore.choerbli().state === ChoerbliStatus.ASSIGNING) {
+      this.phaseApiService.toFinishPhase(this.id).pipe(finalize(() => this.choerbliStore.loadChoerbliById(this.id)),).subscribe(() => {
+      });
+    }
   }
 
   protected readonly ChoerbliStatus = ChoerbliStatus;
