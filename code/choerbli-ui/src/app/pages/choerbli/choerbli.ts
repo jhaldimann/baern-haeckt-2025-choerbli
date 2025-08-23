271ff3324c39ad5563fd13ed41ee739e1c9d7776
Julian Haldimann <julian@haldimann.be>
2025-08-23 15:46:22 +0200

    Create some services and models.

--- code/choerbli-ui/src/app/pages/choerbli/choerbli.ts
+++ code/choerbli-ui/src/app/pages/choerbli/choerbli.ts
@@ -1,64 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Item} from './item/item';
-import {LowerCasePipe} from '@angular/common';
+import {AsyncPipe, LowerCasePipe} from '@angular/common';
+import {ItemDescriptionApiService} from '../../shared/services/item-description-api.service';
+import {BehaviorSubject, Observable, of, take} from 'rxjs';
+import {ItemDescription} from '../../shared/models/item-description.model';
 
 @Component({
   selector: 'app-choerbli',
   imports: [
     Item,
-    LowerCasePipe
+    LowerCasePipe,
+    AsyncPipe
   ],
   templateUrl: './choerbli.html',
   styleUrl: './choerbli.scss'
 })
 export class Choerbli implements OnInit {
+  itemDescriptionApiService: ItemDescriptionApiService = inject(ItemDescriptionApiService);
+  items: BehaviorSubject<ItemDescription[]> = new BehaviorSubject<ItemDescription[]>([]);
   id: string = '';
-  items: any = [
-    {
-      itemId: 1,
-      itemName: 'Beer',
-      category: 'Drinks'
-    },
-    {
-      itemId: 2,
-      itemName: 'Chips',
-      category: 'Snacks'
-    },
-    {
-      itemId: 3,
-      itemName: 'Apple',
-      category: 'Fruits'
-    },
-    {
-      itemId: 4,
-      itemName: 'Flipps',
-      category: 'Snacks'
-    },
-    {
-      itemId: 5,
-      itemName: 'Juice',
-      category: 'Drinks'
-    },
-    {
-      itemId: 6,
-      itemName: 'Pears',
-      category: 'Fruits'
-    },
-    {
-      itemId: 3,
-      itemName: 'Strawberries',
-      category: 'Fruits'
-    }
-  ];
 
   ngOnInit(): void {
     this.id = window.location.pathname.split('/')[2];
+    this.itemDescriptionApiService.getItemDescription().pipe(take(1)).subscribe(data => {
+      this.items.next(data);
+    });
   }
 
   getAllItemCategories(items: any): Set<string> {
     let categories: Set<string> = new Set<string>();
     items.forEach((item: any): void => {
-      categories.add(item.category);
+      categories.add(item.category.name);
     })
     return categories;
   }
---
+++ code/choerbli-ui/src/app/shared/services/item-description-api.service.ts
@@ -0,0 +1,16 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ItemDescription} from '../models/item-description.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemDescriptionApiService {
+  private http: HttpClient = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:8080';
+
+  getItemDescription(): Observable<ItemDescription[]> {
+    return this.http.get<ItemDescription[]>(this.baseUrl+'/api/item/descriptions');
+  }
+}
